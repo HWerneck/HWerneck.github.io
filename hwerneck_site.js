f42856dbfa2306be358b91f6d653d44f5b76cf89
HWerneck <werneck.henrique@outlook.com>
2022-06-25 22:49:47 -0300

    Commit final reorganizando a página.

--- hwerneck_site.js
+++ hwerneck_site.js
@@ -1,4 +1,23 @@
+/*document.addEventListener("ClickOutsideProjectsMenu",
+    function (event) {
+        event.
+    }
+);*/
+
+function displayProjectsMenu() {
+    var menu = document.querySelector(".dropdown-menu");
+    menu.setAttribute("style", "display: block;");
+    return;
+}
+
 /*function displayProjectsMenu() {
-    document.querySelector(".dropdown-menu").setAttribute("display", "block");
+    var menu = document.querySelector(".dropdown-menu");
+    var menu_style = menu.getAttribute("style");
+    if (menu_style.includes("display: block;") || (menu_style.includes("display:block;")) {
+        menu.setAttribute("style", "display: none;");
+    }
+    else {
+        menu.setAttribute("style", "display: block;");
+    }
     return;
 }*/
