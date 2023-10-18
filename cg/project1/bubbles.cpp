be0a96a88fdd5c0cdc46e94800d2e0e6681b8b68
HWerneck <werneck.henrique@outlook.com>
2023-10-18 01:04:16 -0300

    project updated and cleaned

--- cg/project1/bubbles.cpp
+++ cg/project1/bubbles.cpp
@@ -10,9 +10,6 @@ void Bubbles::create(GLuint program, int quantity, int a_speed) {
 
   m_program = program;
 
-  // Get location of uniforms in the program
-  /* m_colorLoc = abcg::glGetUniformLocation(m_program, "color"); */
-  /* m_rotationLoc = abcg::glGetUniformLocation(m_program, "rotation"); */
   m_scaleLoc = abcg::glGetUniformLocation(m_program, "scale");
   m_translationLoc = abcg::glGetUniformLocation(m_program, "translation");
 
@@ -24,10 +21,8 @@ void Bubbles::create(GLuint program, int quantity, int a_speed) {
     bubble = makeBubble(a_speed);
 
     // Make sure the asteroid won't collide with the ship
-    /* do { */
-      bubble.m_translation = {m_randomDist(m_randomEngine),
+    bubble.m_translation = {m_randomDist(m_randomEngine),
                                 m_randomDist(m_randomEngine)};
-    /* } while (glm::length(bubble.m_translation) < 0.5f); */
   }
 }
 
@@ -39,7 +34,6 @@ void Bubbles::paint() {
 
     abcg::glUniform4fv(m_colorLoc, 1, &bubble.m_color.r);
     abcg::glUniform1f(m_scaleLoc, bubble.m_scale);
-    abcg::glUniform1f(m_rotationLoc, bubble.m_rotation);
 
     for (auto i : {0}) {
       abcg::glUniform2f(m_translationLoc, bubble.m_translation.x,
@@ -63,16 +57,8 @@ void Bubbles::destroy() {
 void Bubbles::update(float deltaTime) {
   for (auto &bubble : m_bubbles) {
    glm::vec2 const translation{0.0f, bubble.m_bubblespeed};
-   /* asteroid.m_translation -= ship.m_velocity * deltaTime; */
-    bubble.m_rotation = glm::wrapAngle(
-        bubble.m_rotation + bubble.m_angularVelocity * deltaTime);
     bubble.m_translation += translation * deltaTime;
 
-    // Wrap-around
-    /* if (bubble.m_translation.x < -1.0f) */
-    /*   bubble.m_translation.x += 1.2f; */
-    /* if (bubble.m_translation.x > +1.0f) */
-    /*   bubble.m_translation.x -= 1.2f; */
     if (bubble.m_translation.y < -1.5f)
       bubble.m_translation.y = 1.5f;
     if (bubble.m_translation.y > +1.5f)
@@ -94,21 +80,17 @@ Bubbles::Bubble Bubbles::makeBubble(int speed, glm::vec2 translation,
                          rd(m_randomEngine)};
 
   // Randomly pick the number of sides
-  /* std::uniform_int_distribution randomSides(6, 20); */
-  bubble.m_polygonSides = 20; //randomSides(re);
+  bubble.m_polygonSides = 20; 
 
   // Get a random color (actually, a grayscale)
   std::uniform_real_distribution randomIntensity(0.5f, 1.0f);
   bubble.m_color = glm::vec4(randomIntensity(re));
-  std::uniform_real_distribution randomSpeed(0.05f, 0.15f);
-  bubble.m_bubblespeed = (float) speed/1000 + randomSpeed(re) - 0.10f;
+  std::uniform_real_distribution randomSpeed(- 0.8f, 0.8f);
+  bubble.m_bubblespeed = (float) speed/1000 + randomSpeed(re);
   bubble.m_color.a = 1.0f;
-  bubble.m_rotation = 0.0f;
   bubble.m_scale = scale;
   bubble.m_translation = translation;
 
-  // Get a random angular velocity
-  bubble.m_angularVelocity = m_randomDist(re);
 
   // Get a random direction
   glm::vec2 const direction{m_randomDist(re), m_randomDist(re)};
@@ -118,14 +100,6 @@ Bubbles::Bubble Bubbles::makeBubble(int speed, glm::vec2 translation,
   std::vector<glm::vec2> positions{{0, 0}};
   std::vector<glm::vec3> colors;
   colors.push_back(color1);
-  /* auto const step{M_PI * 2 / bubble.m_polygonSides}; */
-  /* std::uniform_real_distribution randomRadius(0.8f, 1.0f); */
-  /* for (auto const angle : iter::range(0.0, M_PI * 2, step)) { */
-  /*   auto const radius{randomRadius(re)}; */
-  /*   positions.emplace_back(radius * std::cos(angle), radius * std::sin(angle)); */
-  /* } */
-  /* positions.push_back(positions.at(1)); */
-  // Border vertices
   auto const step{M_PI * 2 / bubble.m_polygonSides};
   for (auto const angle : iter::range(0.0, M_PI * 2, step)) {
     positions.emplace_back(std::cos(angle), std::sin(angle));
